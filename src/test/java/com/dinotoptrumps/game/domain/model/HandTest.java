ac72e8996ab7dfec1b749eaaa3d52f22536476a6
Josh Wright <joshua.wright@ada.ac.uk>
2026-03-09 16:34:38 +0000

    Implement domain unit tests for Hand, EloService, DeckService and StatComparisonService

--- src/test/java/com/dinotoptrumps/game/domain/model/HandTest.java
+++ src/test/java/com/dinotoptrumps/game/domain/model/HandTest.java
@@ -2,7 +2,6 @@ package com.dinotoptrumps.game.domain.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,8 +15,8 @@ class HandTest {
         UUID card2 = UUID.randomUUID();
         Hand hand = new Hand(List.of(card1, card2));
 
-        // TODO: Assert topCard() returns card1
-        // TODO: Assert hand still has 2 cards (topCard doesn't remove)
+        assertEquals(card1, hand.topCard());
+        assertEquals(2, hand.size());
     }
 
     @Test
@@ -26,9 +25,9 @@ class HandTest {
         UUID card2 = UUID.randomUUID();
         Hand hand = new Hand(List.of(card1, card2));
 
-        // TODO: Assert removeTopCard() returns card1
-        // TODO: Assert hand now has 1 card
-        // TODO: Assert topCard() now returns card2
+        assertEquals(card1, hand.removeTopCard());
+        assertEquals(1, hand.size());
+        assertEquals(card2, hand.topCard());
     }
 
     @Test
@@ -40,31 +39,37 @@ class HandTest {
 
         hand.addCardsToBottom(List.of(card2, card3));
 
-        // TODO: Assert hand has 3 cards
-        // TODO: Assert topCard() is still card1 (new cards went to bottom)
+        assertEquals(3, hand.size());
+        assertEquals(card1, hand.topCard());
     }
 
     @Test
     void emptyHand_topCardThrows() {
         Hand hand = Hand.empty();
-        // TODO: Assert that calling topCard() throws IllegalStateException
+        assertThrows(IllegalStateException.class, hand::topCard);
     }
 
     @Test
     void emptyHand_removeTopCardThrows() {
         Hand hand = Hand.empty();
-        // TODO: Assert that calling removeTopCard() throws IllegalStateException
+        assertThrows(IllegalStateException.class, hand::removeTopCard);
     }
 
     @Test
     void isEmpty_returnsTrueForEmptyHand() {
-        // TODO: Test isEmpty() on an empty hand and a non-empty hand
+        UUID card1 = UUID.randomUUID();
+        Hand emptyHand = Hand.empty();
+        Hand notEmptyHand = new Hand(List.of(card1));
+        assertTrue(emptyHand.isEmpty());
+        assertFalse(notEmptyHand.isEmpty());
     }
 
     @Test
     void getCardIds_returnsUnmodifiableList() {
         Hand hand = new Hand(List.of(UUID.randomUUID()));
-        // TODO: Assert that trying to add to getCardIds() throws an exception
-        // This verifies the list is truly unmodifiable
+        List<UUID> ids = hand.getCardIds();
+        assertThrows(Exception.class, () -> {
+            ids.add(hand.topCard());
+        });
     }
 }
--- src/test/java/com/dinotoptrumps/game/domain/service/DeckServiceTest.java
+++ src/test/java/com/dinotoptrumps/game/domain/service/DeckServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,47 +24,60 @@ class DeckServiceTest {
     void deal_evenNumberOfCards_splitEqually() {
         List<UUID> cards = createCardIds(10);
         Hand[] hands = deckService.deal(cards);
-        // TODO: Assert both hands have exactly 5 cards
+        assertEquals(5, hands[0].size());
+        assertEquals(hands[0].size(), hands[1].size());
     }
 
     @Test
     void deal_oddNumberOfCards_player1GetsExtra() {
         List<UUID> cards = createCardIds(11);
         Hand[] hands = deckService.deal(cards);
-        // TODO: Assert player1 has 6 cards and player2 has 5
+        assertEquals(6, hands[0].size());
+        assertEquals(5, hands[1].size());
     }
 
     @Test
     void deal_allCardsAreDistributed() {
         List<UUID> cards = createCardIds(20);
         Hand[] hands = deckService.deal(cards);
-        // TODO: Assert that hands[0].size() + hands[1].size() equals 20
-        // All cards should be accounted for
+        assertEquals(20, hands[0].size() + hands[1].size());
     }
 
     @Test
     void deal_noCardAppearsInBothHands() {
         List<UUID> cards = createCardIds(10);
         Hand[] hands = deckService.deal(cards);
-        // TODO: Assert no card ID appears in both hands
-        // Hint: check that the intersection of both card lists is empty
+        List<UUID> handOne = hands[0].getCardIds();
+        List<UUID> handTwo = hands[1].getCardIds();
+
+        HashSet<UUID> combinedHands = new HashSet<>();
+        combinedHands.addAll(handOne);
+        combinedHands.addAll(handTwo);
+        assertEquals(10, combinedHands.size());
     }
 
     @Test
     void deal_cardsAreShuffled() {
-        // This is tricky to test since shuffling is random
-        // TODO: Deal the same cards multiple times and verify that
-        // the order isn't always the same (run 10 deals, check at least
-        // one differs from the first)
+        List<UUID> cards = createCardIds(10);
+        Hand[] base = deckService.deal(cards);
+        List<UUID> handOne = base[0].getCardIds();
+        boolean foundDifference = false;
+        for (int i = 0; i < 9; i++) {
+            Hand[] variant = deckService.deal(cards);
+            if (!variant[0].getCardIds().equals(handOne)) {
+                foundDifference = true;
+            }
+        }
+        assertTrue(foundDifference);
     }
 
     @Test
     void deal_emptyDeck_returnsTwoEmptyHands() {
         Hand[] hands = deckService.deal(new ArrayList<>());
-        // TODO: Assert both hands are empty
+        assertTrue(hands[0].isEmpty());
+        assertTrue(hands[1].isEmpty());
     }
 
-    // Helper method to create a list of random card IDs
     private List<UUID> createCardIds(int count) {
         List<UUID> ids = new ArrayList<>();
         for (int i = 0; i < count; i++) {
